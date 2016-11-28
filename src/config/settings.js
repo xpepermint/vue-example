19778580d3899f6fc9d4a78b86277c93ec07b25a
xpeper <xpepermint@gmail.com>
2016-11-28 01:40:18 +0100

    heroku configuration

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  env: process.env.npm_package_config_env,
-  serverPort: process.env.npm_package_config_serverPort,
-  serverHost: process.env.npm_package_config_serverHost,
-  publicPath: process.env.npm_package_config_publicPath
+  env: process.env.NODE_ENV || process.env.npm_package_config_env,
+  serverPort: process.env.PORT || process.env.npm_package_config_serverPort,
+  serverHost: process.env.HOST || process.env.npm_package_config_serverHost,
+  publicPath: process.env.PUBLIC_PATH || process.env.npm_package_config_publicPath
 };
